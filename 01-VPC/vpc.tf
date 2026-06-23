157fd612a0f9c0d5c7228a41c451eb1594f6dc48
Fabian <fab27fc@gmail.com>
2026-06-23 17:11:27 -0600

    Add VPC Terraform lab

--- 01-VPC/vpc.tf
+++ 01-VPC/vpc.tf
@@ -9,18 +9,22 @@ terraform {
 
 resource "aws_vpc" "demo_vpc" {
     cidr_block = "10.0.0.0/16"
+
+    tags = {
+        Name = "demo_vpc"
+    }
 }
 
 resource "aws_subnet" "public_subnet" {
     vpc_id            = aws_vpc.demo_vpc.id
     cidr_block        = "10.0.0.0/24"
-    availability_zone = "us-east-1a" 
+    availability_zone = "us-east-2a" 
 }
 
 resource "aws_subnet" "private_subnet" {
     vpc_id            = aws_vpc.demo_vpc.id
     cidr_block        = "10.0.1.0/24"
-    availability_zone = "us-east-1b" 
+    availability_zone = "us-east-2b" 
 }
 
 resource "aws_internet_gateway" "igw" {
@@ -35,12 +39,12 @@ resource "aws_route_table" "public_route_table" {
         gateway_id = aws_internet_gateway.igw.id
     }
 }
-
+# "Firsrt string" name of reousrce and "Second string" is the name that I put in the resource. 
 resource "aws_route_table_association""public_route_table_association" {
     subnet_id      = aws_subnet.public_subnet.id
     route_table_id = aws_route_table.public_route_table.id
 }
 
 provider "aws" {
-    region = "us-east-1"
+    region = "us-east-2"
 }
