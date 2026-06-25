724f5b1df6508e158a8ca68390ecc115d495fd02
Fabian <fab27fc@gmail.com>
2026-06-25 17:56:07 -0600

    Add Terraform CLI lab documentation

--- 03-First-tf-Project/providers.tf
+++ 03-First-tf-Project/providers.tf
@@ -16,15 +16,3 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "random_id" "bucket_suffix" {
-  byte_length = 6
-}
-
-resource "aws_s3_bucket" "my_bucket_example" {
-  bucket = "my-bucket-${random_id.bucket_suffix.hex}"
-}
-
-output "bucket_name" {
-  value = aws_s3_bucket.my_bucket_example.bucket
-}
-
